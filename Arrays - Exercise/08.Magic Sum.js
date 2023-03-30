b6da516f30793b103c54e38ec677f3e7fcb2424a
op46s <operator46@icloud.com>
2023-03-30 20:17:17 +0100

    Home

---
+++ Arrays - Exercise/08.Magic Sum.js
@@ -0,0 +1,19 @@
+function magicSum(arr, num) {
+
+    let newArr = [];
+
+    for (let i=0; i<arr.length; i++) {
+
+        for(let j=i+1;j<arr.length; j++) {
+
+            if (arr[i]+arr[j] == num) {
+            newArr.push(arr[i], arr[j]); 
+            console.log(newArr.join(' '));
+            newArr = [];
+            }
+        }
+    }
+}
+//magicSum([1, 7, 6, 2, 19, 23], 8);
+//magicSum([14, 20, 60, 13, 7, 19, 8], 27);
+//magicSum([1, 2, 3, 4, 5, 6], 6);

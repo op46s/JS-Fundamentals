4c2567ddcacac7daa14730df4aaa07c2ac90001b
op46s <operator46@icloud.com>
2023-04-01 07:58:35 +0100

    Home

--- MID EXAMS/01. Programming Fundamentals Mid Exam Retake/01.Computer Store.js
+++ MID EXAMS/01. Programming Fundamentals Mid Exam Retake/01.Computer Store.js
@@ -31,6 +31,6 @@ function computerStore(inputPrices) {
     console.log(`Total price: ${totalPriceWithTaxes.toFixed(2)}$`);
 
 }
-//computerStore(['1050', '200', '450', '2', '18.50', '16.86', 'special']);
+computerStore(['1050', '200', '450', '2', '18.50', '16.86', 'special']);
 //computerStore(['1023', '15', '-20', '-5.50', '450', '20', '17.66', '19.30', 'regular']);
-computerStore(['regular']);
+//computerStore(['regular']);

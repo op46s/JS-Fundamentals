952552339e742e2bebabc93c1012b4d2521e4475
op46s <operator46@icloud.com>
2023-04-11 20:28:20 +0100

    Home

--- Functions - Lab/05. Simple Calculator.js
+++ Functions - Lab/05. Simple Calculator.js
@@ -1,22 +1,23 @@
 function simpleCalculator(a, b, operator) {
 
-    // let divide = (a, b) => a / b;
-    // let multiplyMe = (a, b) => a * b;
-    // let add = (a, b) => a + b;
-    // let subtract = (a, b) => a * b; 
+    const calculate = (num1, num2, operator) => ({
+        'multiply': () => num1 * num2,
+        'divide': () => num1 / num2,
+        'add': () => num1 + num2,
+        'subtract': () => num1 - num2
+      })[operator]();
 
-    // console.log(divide(a, b));
+    console.log(calculate(a, b, operator));
+    
+    // let result = 0;
 
-
-    let result = 0;
-
-    switch (operator) {
-        case 'multiply': result = a * b; break;
-        case 'divide': result = a / b; break;
-        case 'add': result = a + b; break;
-        case 'subtract': result = a - b; break;
-    }
-    console.log(result);
+    // switch (operator) {
+    //     case 'multiply': result = a * b; break;
+    //     case 'divide': result = a / b; break;
+    //     case 'add': result = a + b; break;
+    //     case 'subtract': result = a - b; break;
+    // }
+    // console.log(result);
 }
 simpleCalculator(5, 5, 'multiply');
 simpleCalculator(40, 8, 'divide');

0fab5509f2333e0e4e114fa60fcd3e8dea3563c8
op46s <operator46@icloud.com>
2023-04-11 15:17:07 +0100

    Office

---
+++ .Codewars/Printer Errors.js
@@ -0,0 +1,16 @@
+function printerError(s) {
+  
+    function printerError(s) {
+        let errorCounter = 0;
+        let inputToArray = s.split('');
+        for (let i=0; i<inputToArray.length; i++) {
+          if (inputToArray[i].charCodeAt(0) < 97 || inputToArray[i].charCodeAt(0) > 109) {
+              errorCounter++;
+          }
+        }
+        return `${errorCounter}/${inputToArray.length}`;
+      }
+      printerError('aaabbbbhaijjjm')
+}
+printerError('a');
+

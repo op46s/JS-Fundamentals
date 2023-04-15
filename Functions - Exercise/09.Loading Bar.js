c282dc876834d7fdc472bff57c89a26c368b1ef2
op46s <operator46@icloud.com>
2023-04-15 17:36:49 +0100

    Home

---
+++ Functions - Exercise/09.Loading Bar.js
@@ -0,0 +1,13 @@
+function loadingBar(n) {
+
+    if (n == 100) {
+        console.log('100% Complete!');
+        console.log('[%%%%%%%%%%]');
+    } else {
+        console.log(`${n}% `+'['+'%'.toString().repeat(n/10) + '.'.toString().repeat(10-n/10)+']');
+        console.log('Still loading...');
+    }
+}
+loadingBar(90);
+//loadingBar(50);
+//loadingBar(100);

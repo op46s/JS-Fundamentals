bab42a9a914b2ab3e4d4c55d709b4b2cb036861a
op46s <operator46@icloud.com>
2023-05-07 16:24:21 +0100

    man o war

--- MID EXAMS/06. Programming Fundamentals Mid Exam Retake/03. Man O War.js
+++ MID EXAMS/06. Programming Fundamentals Mid Exam Retake/03. Man O War.js
@@ -1,4 +1,4 @@
-function manOWar(arr) { 
+function manOWar(arr) {
 
     let pirateShip = arr.shift().split('>').map(Number);
     let warShip = arr.shift().split('>').map(Number);
@@ -11,44 +11,44 @@ function manOWar(arr) {
         let element = commands.split(' ')
 
         switch (element[0]) {
-            case 'Fire' :
+            case 'Fire':
                 if (Number(element[1]) >= 0 && Number(element[1]) < warShip.length) {
-                    warShip[Number(element[1])]-= Number(element[2]);
+                    warShip[Number(element[1])] -= Number(element[2]);
                     if (warShip[Number(element[1])] <= 0) {
                         console.log('You won! The enemy ship has sunken.')
                         return;
                     }
                 }
-            break;
-            case 'Defend' :
+                break;
+            case 'Defend':
                 if ((Number(element[1]) >= 0 && Number(element[1]) < pirateShip.length) && (Number(element[2]) >= 0 && Number(element[2]) < pirateShip.length)) {
-                    for (let i=Number(Number(element[1])); i<=Number(element[2]); i++) {
-                        pirateShip[i]-= Number(element[3]);
-                            if (pirateShip[i] <=0) {
-                                console.log('You lost! The pirate ship has sunken.');
-                                return;
-                            }
+                    for (let i = Number(Number(element[1])); i <= Number(element[2]); i++) {
+                        pirateShip[i] -= Number(element[3]);
+                        if (pirateShip[i] <= 0) {
+                            console.log('You lost! The pirate ship has sunken.');
+                            return;
+                        }
                     }
                 }
-            break;
-            case 'Repair' : 
+                break;
+            case 'Repair':
                 if (Number(element[1]) >= 0 && Number(element[1]) < pirateShip.length) {
-                    pirateShip[Number(element[1])]+=Number(element[2]);
-                    if(pirateShip[Number(element[1])] > maxHealth) {
+                    pirateShip[Number(element[1])] += Number(element[2]);
+                    if (pirateShip[Number(element[1])] > maxHealth) {
                         pirateShip[Number(element[1])] = maxHealth;
                     }
                 }
 
-            break;
-            case 'Status' : 
+                break;
+            case 'Status':
                 let repairCounter = 0;
-                for (let i=0; i<pirateShip.length; i++) {
+                for (let i = 0; i < pirateShip.length; i++) {
                     if (pirateShip[i] < repairCheck) {
                         repairCounter++;
                     }
                 }
                 console.log(`${repairCounter} sections need repair.`);
-            break;
+                break;
         }
         commands = arr.shift();
     }
@@ -60,23 +60,23 @@ function manOWar(arr) {
     console.log(`Warship status: ${warShipResult}`);
 }
 manOWar(["12>13>11>20>66",
-        "12>22>33>44>55>32>18",
-        "70",
-        "Fire 2 11",
-        "Fire 8 100",
-        "Defend 3 6 11", // index index damage
-        "Defend 0 3 5",
-        "Repair 1 33",
-        "Status",
-        "Retire"
+    "12>22>33>44>55>32>18",
+    "70",
+    "Fire 2 11",
+    "Fire 8 100",
+    "Defend 3 6 11", // index index damage
+    "Defend 0 3 5",
+    "Repair 1 33",
+    "Status",
+    "Retire"
 ]);
 console.log('....................................')
 manOWar(["2>3>4>5>2",
-        "6>7>8>9>10>11",
-        "20",
-        "Status",
-        "Fire 2 3",
-        "Defend 0 4 11",
-        "Repair 3 18",
-        "Retire"
+    "6>7>8>9>10>11",
+    "20",
+    "Status",
+    "Fire 2 3",
+    "Defend 0 4 11",
+    "Repair 3 18",
+    "Retire"
 ]);

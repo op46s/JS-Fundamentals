a6ca4b9dfaa1ce46009c892610fb21f40d816668
op46s <operator46@icloud.com>
2023-05-10 19:57:12 +0100

    Fixed problem 06

--- Arrays Advanced - Exercise/06.Bomb Numbers.js
+++ Arrays Advanced - Exercise/06.Bomb Numbers.js
@@ -2,28 +2,23 @@ function bombNumbers(myArr, special) {
 
     let bombNumber = special[0];
     let power = special[1];
-    let bNum = myArr.indexOf(bombNumber);
-    let detonatorCount = 0;
 
-    for (const el of myArr) {
-        if (el == bombNumber) {
-            detonatorCount++;
-        }
-    }
+        for (let i=0; i<myArr.length; i++) {
+            let checker = myArr[i];
+                if (checker === bombNumber) {
 
-    if (detonatorCount == 1) {
-        myArr.splice(bNum-power, bNum+power);
-        console.log(myArr.reduce((a, b) => a+b));
-    } else {
-        while (myArr.indexOf(bombNumber) != -1) {
-           
-            myArr.splice(bNum-power, bNum+power);
-            
-        }
-        console.log(myArr.reduce((a, b) => a+b))
-    }
+                    let startIndex = Math.max(0, i-power)
+                    let deleteCount = power * 2 + 1
+                    myArr.splice(startIndex, deleteCount)
+                }
+        }        
+        console.log(myArr.reduce((a, b) => a + b, 0));
 }
 bombNumbers([1, 2, 2, 4, 2, 2, 2, 9], [4, 2]);
 bombNumbers([1, 4, 4, 2, 8, 9, 1], [9, 3]);
 bombNumbers([1, 7, 7, 1, 2, 3], [7, 1]);
 bombNumbers([1, 1, 2, 1, 1, 1, 2, 1, 1, 1], [2, 1]);
+bombNumbers([1, 1, 1], [1, 0]);
+
+
+

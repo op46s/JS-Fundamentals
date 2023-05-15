5de657c89f68339d720750d5fbb0ab5406239694
op46s <operator46@icloud.com>
2023-05-15 18:07:01 +0100

    Home

---
+++ Objects and Classes - Lab/01.Person Info.js
@@ -0,0 +1,16 @@
+function personInfo(fName, lName, age) {
+
+    let person = {
+        // firstName: fName,
+        // lastName: lName,
+        // age: age
+    }
+    person.firstName = fName;
+    person.lastName = lName;
+    person.age = age
+   console.log(person);
+}
+personInfo("Peter", 
+"Pan",
+"20"
+)

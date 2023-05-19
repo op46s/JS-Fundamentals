471cffbfb8c326386e9eb085330a9b66b7c1a935
op46s <operator46@icloud.com>
2023-05-19 17:17:48 +0100

    Home

--- Data Types and Variables - Lab/06. Gramophone.js
+++ Data Types and Variables - Lab/06. Gramophone.js
@@ -1,11 +1,9 @@
 function gramophone(band, album, song) {
-
-    let result = ((band.length * album.length) * song.length / 2) / 2.5;
-    console.log(`The plate was rotated ${Math.ceil(result)} times.`);
-
+  let result = (band.length * album.length * song.length) / 2 / 2.5;
+  console.log(`The plate was rotated ${Math.ceil(result)} times.`);
 }
-gramophone('Black Sabbath', 'Paranoid', 'War Pigs');
-gramophone('Rammstein', 'Sehnsucht', 'Engel');
 
+gramophone("Black Sabbath", "Paranoid", "War Pigs");
+gramophone("Rammstein", "Sehnsucht", "Engel");
 
 //(albumName.length * bandName.length) * song-name.length / 2

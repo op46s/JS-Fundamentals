c985ac9bccb126202d5abcd47a0ac10e3b729a45
op46s <operator46@icloud.com>
2023-05-19 17:05:56 +0100

    Home

--- Data Types and Variables - Lab/10. Triples of Latin Letters.js
+++ Data Types and Variables - Lab/10. Triples of Latin Letters.js
@@ -1,15 +1,15 @@
 function tripleLatinLetters(num) {
 
- for (let loop1 = 0; loop1 < num; loop1++) {
-    let letter1 = String.fromCharCode(97 + loop1);
-    for (let loop2 = 0; loop2 < num; loop2++) {
-        let letter2 = String.fromCharCode(97 + loop2);
-     
-        for (let loop3 = 0; loop3 < num; loop3++) {
-            let letter3 = String.fromCharCode(97 + loop3);
-            console.log(letter1 + letter2 + letter3);
-        } 
+    for (let loop1 = 0; loop1 < num; loop1++) {
+        let letter1 = String.fromCharCode(97 + loop1);
+        for (let loop2 = 0; loop2 < num; loop2++) {
+            let letter2 = String.fromCharCode(97 + loop2);
+
+            for (let loop3 = 0; loop3 < num; loop3++) {
+                let letter3 = String.fromCharCode(97 + loop3);
+                console.log(letter1 + letter2 + letter3);
+            }
+        }
     }
- }
 }
 tripleLatinLetters(2);

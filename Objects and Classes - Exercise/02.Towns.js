4fad774debb1e4f5ddbdad9563874b9e69268d98
op46s <operator46@icloud.com>
2023-05-19 17:15:06 +0100

    Home

--- Objects and Classes - Exercise/02.Towns.js
+++ Objects and Classes - Exercise/02.Towns.js
@@ -1,18 +1,18 @@
 function towns(arr) {
+  let collectTownData = [];
 
-    let collectTownData = [];
+  for (let i = 0; i < arr.length; i++) {
+    let [town, latitude, longitude] = arr[i].split(" | ");
+    collectTownData.push({
+      town: town,
+      latitude: Number(latitude).toFixed(2),
 
-    for(let i=0; i<arr.length; i++) {
-        let [town, latitude, longitude] = arr[i].split(' | ')
-        collectTownData.push({
-            town: town,
-            latitude: (Number(latitude).toFixed(2)),
-            longitude: (Number(longitude).toFixed(2))
-        }) 
-    }
-    for (let obj of collectTownData) {
-        console.log(obj);
-    }
+      longitude: Number(longitude).toFixed(2),
+    });
+  }
+  for (let obj of collectTownData) {
+    console.log(obj);
+  }
 }
-towns(['Sofia | 42.696552 | 23.32601', 'Beijing | 39.913818 | 116.363625']);
+towns(["Sofia | 42.696552 | 23.32601", "Beijing | 39.913818 | 116.363625"]);
 //towns(['Plovdiv | 136.45 | 812.575']);
